37fe762479e6f90b3d705f1a54c8cc7048012c9f
Nastya Tychyna <43435254+ntchna@users.noreply.github.com>
2020-06-17 12:11:45 +0300

    Delete InvalidCompanyException.java

--- src/com/KPI/IS81/lab01/exception/InvalidCompanyException.java
+++
@@ -1,9 +0,0 @@
-package com.KPI.IS81.lab01.exception;
-
-import java.io.IOException;
-
-public class InvalidCompanyException extends IOException {
-    public InvalidCompanyException(String message){
-        super(message);
-    }
-}
