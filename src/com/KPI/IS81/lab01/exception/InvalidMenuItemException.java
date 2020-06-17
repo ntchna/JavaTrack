e300e8edfdd4ce05c5ff06f6839deeb2e2e22715
Nastya Tychyna <43435254+ntchna@users.noreply.github.com>
2020-06-17 12:11:52 +0300

    Delete InvalidMenuItemException.java

--- src/com/KPI/IS81/lab01/exception/InvalidMenuItemException.java
+++
@@ -1,9 +0,0 @@
-package com.KPI.IS81.lab01.exception;
-
-import java.io.IOException;
-
-public class InvalidMenuItemException extends IOException {
-    public InvalidMenuItemException(String message) {
-        super(message);
-    }
-}
